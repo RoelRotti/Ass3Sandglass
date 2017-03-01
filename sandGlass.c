99bd68ebd0062d760a8ce40bf75922ce912f4099
Mathijs <mathew9753@gmail.com>
2017-03-01 12:45:16 +0100

    add sandGlass.c

---
+++ sandGlass.c
@@ -0,0 +1,16 @@
+/* file: sandGlass.c                                 		*/
+/* author: 	A.M van Maurik (email: mathew9753@gmail.com) 	*/
+/*			Roel Rotteveel									*/
+/* date: 1-03-2017                                     		*/
+/* version:                                        			*/
+/* Description: 											*/
+
+
+
+int main(int argc, char *argv[]) {
+
+
+
+
+	return 0;
+}
